9c0ecade88e0cf69398482900209268674a36b4c
aghasilorsabyanmic <aghasi.lorsabyan@mic.am>
2016-02-28 19:23:43 +0400

    Ajax Seach

---
+++ MvcSearch/MvcSearch/Models/ModelFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcSearch.Models
+{
+    public class ModelFactory
+    {
+        public static IEnumerable<ProductViewModel> GetProducts()
+        {
+            var result = Enumerable.Range(1, 100).Select(n => new ProductViewModel {
+                Name = $"Product {n}",
+                Price = n * 10
+            });
+
+            return result;
+        }
+    }
+}
---
+++ MvcSearch/MvcSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcSearch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcSearch")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b530e28-2db1-4736-8ef3-9a59af90af76")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
